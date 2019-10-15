a5b5f8af6647f71b825dec3a80bebd401c460805
Notu <notyoudev@gmail.com>
2019-10-15 14:19:18 -0400

    Work On fixing Spagat (2/5)

--- python/commands.py
+++ python/commands.py
@@ -1,6 +1,12 @@
 def cmdName():
 	cmdName = input("Display name: ")
 
+def cmdGuild():
+	cmdGuild = input("Name of server: ")
+	for guild in client.guilds:
+		if cmdGuild == guild.name:
+			print("Now Ported to ["+cmdGuild+"]")
+
 def cmdMessage():
 	cmdMessage = input("Message: ")
 	botMessage = ("[" + cmdName + "]: " + cmdMessage)
@@ -8,12 +14,13 @@ def cmdMessage():
 
 def cmdChannel():
 	cmdChannel = input("Channel: ")
+	for channel in guild.channels:
+		print(channel)
+		for channel in guild.channels:
+			if channel == channel.name:
+				print("Now chatting in ["+cmdChannel+"]")
+				# get channel message hsitory here
 
-def cmdGuild():
-	cmdGuild = input("Name of server: ")
-	for guild in client.guilds:
-		if cmdGuild == guild.name:
-			print("Now chatting in ["+cmdGuild+"]")
 
 def cmdCommands():
 	cmdCommand = input("Command: ")
