3eaca4d04978d09c3fcabeb129b7bcb1653725cb
Notu <notyoudev@gmail.com>
2019-10-15 12:53:20 -0400

    Create commands.py

---
+++ python/commands.py
@@ -0,0 +1,11 @@
+def newName():
+	cmdName = input("Display name: ")
+
+def newMessage():
+	cmdMessage = input("Message: ")
+
+def changeChannel():
+	cmdChannel = input("Channel: ")
+
+def changeGuild():
+	cmdGuild = input("Name of server: ")
