3c9203cfb6b3b8401d31cf8b7c50119214140a5e
notU <notyoudev@gmail.com>
2020-01-01 15:53:43 -0500

    no longer needed?

--- backend/main.py
+++
@@ -1,6 +0,0 @@
-import websocket
-import discord
-
-# this is so-nothing script
-# I have no idea what this is for, but I'm going to focus on the discord side, so I assume that you'll tyake care of this
-# All requests can be handled though the "cmds.py" script on the on_message cog
